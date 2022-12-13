9e6062c9c8999d23a8e82bc4ff85b6fcb8eda07c
Raphael CIAZE <rciaze@e1r8p10.clusters.42paris.fr>
2022-12-13 16:30:32 +0100

    push

--- libft/ft_lstsize.c
+++ libft/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: rciaze <rciaze@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 17:38:19 by rciaze            #+#    #+#             */
-/*   Updated: 2022/11/25 19:53:56 by rciaze           ###   ########.fr       */
+/*   Updated: 2022/12/13 16:30:30 by rciaze           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,3 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (i);
 }
-
- int	main(void)
-{
-	t_list	*liste1;
-	t_list	*liste2;
-	t_list	*liste3;
-	t_list	*liste4;
-
-	liste1 = ft_lstnew((void *) 'a');
-	liste2 = ft_lstnew((void *) 'b');
-	liste3 = ft_lstnew((void *) 'c');
-	liste4 = ft_lstnew((void *) 'd');
-
-	ft_lstadd_front(&liste4, liste3);
-	ft_lstadd_front(&liste3, liste2);
-	ft_lstadd_front(&liste2, liste1);
-	printf("%d", ft_lstsize(liste1));
-} 
